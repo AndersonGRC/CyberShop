1d1fe702a1c0ca4442abf5d079f583406f0a5421
“JobAnderson” <andersongermancavi@gmail.com>
2025-02-12 11:54:57 -0500

    Rutas Header

--- app/app.py
+++ app/app.py
@@ -2,25 +2,6 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-# Ruta principal
-@app.route('/')
-def index():
-    MenuApp = [
-        {"nombre": "Inicio", "url": "index"},
-        {"nombre": "Productos", "url": "#"},
-        {"nombre": "Servicios", "url": "servicios"},
-        {"nombre": "¿Quienes Somos?", "url": "#"},
-        {"nombre": "Contactanos", "url": "#"},
-        {"nombre": "Ingresar", "url": "#"}
-    ]
-    
-    datosApp = {
-        'titulo': 'CyberShop',
-        'MenuAppindex': MenuApp,
-        'longMenuAppindex': len(MenuApp)
-    }
-
-    return render_template('index.html', datosApp=datosApp)
 
 
 # Ruta para cargar otras páginas, asegurando que `datosApp` siempre esté presente
@@ -32,7 +13,7 @@ def pagina(filename):
         {"nombre": "Servicios", "url": "servicios"},
         {"nombre": "¿Quienes Somos?", "url": "#"},
         {"nombre": "Contactanos", "url": "#"},
-        {"nombre": "Ingresar", "url": "#"}
+        {"nombre": "Ingresar", "url": "login"}
     ]
     
     datosApp = {
