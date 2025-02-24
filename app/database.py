37d4564a6361de9ddcbe4f70406c4e0aa7761889
“JobAnderson” <andersongermancavi@gmail.com>
2025-02-23 23:56:15 -0500

    Agregado .gitignore y actualizado database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ import psycopg2
 def get_db_connection():
     conn = psycopg2.connect(
         dbname="cybershop",
-        user="postgres",
+        user="cybershopuser",
         password="Omegafito7217*",
         host="localhost",
         port="5432"
