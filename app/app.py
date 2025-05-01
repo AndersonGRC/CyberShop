9ea983d963bc0fb05b7c2ac5216bda39ecb40ffe
Sofia <sofiagutierrez12062018@gmail.com>
2025-05-01 16:59:04 -0500

    Cambio de contraseña de app

--- app/app.py
+++ app/app.py
@@ -16,7 +16,7 @@ app.config['MAIL_PORT'] = 587  # Puerto para TLS
 app.config['MAIL_DEBUG'] = True
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'cybershop.digitalsales@gmail.com'  # Tu correo Gmail
-app.config['MAIL_PASSWORD'] = 'e y x t f m t i b v b u g k m x'  # Contraseña o App Password de Gmail
+app.config['MAIL_PASSWORD'] = 'u n c x i k z b k u a m e o n g'  # Contraseña o App Password de Gmail
 app.config['MAIL_DEFAULT_SENDER'] = 'yalgomasachiras@gmail.com'  # Correo remitente por defecto
 
 # Inicializa Flask-Mail
